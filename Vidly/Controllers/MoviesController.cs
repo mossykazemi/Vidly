8ac2c8a0d4252bd6a05babafb5051e599f395b28
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-05-04 05:24:50 +0430

    Added attribute routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -21,6 +21,7 @@ namespace Vidly.Controllers
             //return RedirectToAction("Index", "Home",new{page=1, sortBy="name"});
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year,int month)
         {
             return Content(year + "/"+ month);
