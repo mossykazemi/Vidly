f693c3ccbfcafc56783ad9b7cc291a6e8da7cfff
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-05-15 06:45:43 +0430

    Added validation to new movie page validation for server side and client side

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -47,9 +47,8 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
             return View("MovieForm", viewModel);
@@ -88,8 +87,18 @@ namespace Vidly.Controllers
 
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel=new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded=DateTime.Now;
--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
@@ -9,17 +11,45 @@ namespace Vidly.ViewModels
     public class MovieFormViewModel
     {
         public IEnumerable<Genre> Genres { get; set; }
-        public Movie Movie { get; set; }
 
-        public string Title
+        public int? Id { get; set; }
+
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+
+        [Display(Name = "Genre")]
+        [Required]
+        public byte? GenreId { get; set; }
+
+
+        [Display(Name = "Release Date")]
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+
+        [Display(Name = "Number in stock")]
+        [Range(1,20)]
+        [Required]
+        public byte? NumberInStock { get; set; }
+
+
+        public string Title => Id != 0 ? "Edit Movie" : "New Movie";
+
+        public MovieFormViewModel()
         {
-            get
-            {
-                if (Movie != null && Movie.Id != 0)
-                    return "Edit Movie";
+            Id = 0;
+        }
 
-                return "New Movie";
-            }
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
         }
     }
 }
